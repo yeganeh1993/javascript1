f5380e9a818c8be9af0a8bb499c77cbcece0d985
yeganeh1993 <53016837+yeganeh1993@users.noreply.github.com>
2019-09-01 23:18:50 +0300

    Add files via upload

---
+++ week 2/java.js
@@ -0,0 +1,124 @@
+
+// #1
+console.log('Question 1:');
+let english = "Hello world";
+console.log(english);
+let persian = "سلام دنیا";
+console.log(persian);
+
+// #2
+console.log('Question 2:');
+console.log("I'm awesome");
+
+//#3
+console.log('Question 3:');
+let x;
+x=20
+console.log("the value of my variable x will be:  " + x);
+console.log(x);
+
+
+// #4
+console.log('Question 4:');
+let y = "Aref";
+console.log("My name is:  "+y);
+console.log(y);
+
+// #5
+console.log('Question 5:');
+let z = 7.25;
+console.log(z);
+let a = 5.5;
+console.log(a);
+if (z>a) {
+    console.log(z+"  biger then  "+a);
+    
+}else{
+    console.log(z+"  smaller then:  "+a );
+    }
+
+// #6
+console.log('Question 6:');
+let colors=['red','green','blue']
+console.log('my Arrays is abote colors');
+console.log(colors);
+let animals=['dog','cat','hors']
+console.log('my arrays is abote favorite animals');
+console.log(animals);
+animals.push("baby pig");
+console.log(animals);
+
+
+// #7
+console.log('Question 7:');
+let myString = 'THis is a test';
+console.log(myString)
+myString.length
+console.log(myString.length)
+
+// #8
+console.log('Question 8:');
+let FirstName ="Aref"
+console.log('The name of my first variable is: '+FirstName);
+let ArefEge =27
+console.log('Aref age is ' +ArefEge+' years');
+let SecondName ="Yeganeh"
+console.log('The name of my second variable is: '+SecondName);
+let YeganehEge =25
+console.log('Yeganeh age is ' +YeganehEge+' years');
+
+if (typeof FirstName === typeof ArefEge) {
+    console.log('same type')
+}else{
+    console.log('Not same type')
+}
+
+if (typeof FirstName === typeof SecondName) {
+    console.log('same type')
+}else{
+    console.log('Not same type')
+}
+
+if (typeof FirstName === typeof YeganehEge) {
+    console.log('same type')
+}else{
+    console.log('Not same type')
+}
+
+if (typeof ArefEge === typeof SecondName) {
+    console.log('same type')
+}else{
+    console.log('Not same type')
+}
+
+if (typeof ArefEge === typeof YeganehEge) {
+    console.log('same type')
+}else{
+    console.log('Not same type')
+}
+
+if (typeof SecondName === typeof YeganehEge) {
+    console.log('same type')
+}else{
+    console.log('Not same type')
+}
+
+
+
+// #9
+console.log('Question 9:');
+var X=7;
+console.log(X%3);
+
+
+// #10
+console.log('Question 10:');
+if (6/0===10/0){
+    console.log(true)
+
+}else{console.log(false)}
+
+
+
+
+
